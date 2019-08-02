548bf2e8d82a597cbf6b4bc1e9cf170cb8d553f0
xiuxiu zhang <xiuxiuzhang@xiuxiudeMacBook-Air.local>
2019-08-02 17:35:31 +0800

    ddd

--- didi-user/pages/my/index.vue
+++ didi-user/pages/my/index.vue
@@ -1,14 +1,18 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<image-upload></image-upload>
 	</view>
 </template>
 
 <script>
+	
+	import ImageUpload from '../../components/image-upload.vue';
+	
 	export default {
+		components:{
+				ImageUpload
+		},
+		
 		data() {
 			return {
 				title: 'Hello'
@@ -31,22 +35,5 @@
 		justify-content: center;
 	}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50upx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+	
 </style>
