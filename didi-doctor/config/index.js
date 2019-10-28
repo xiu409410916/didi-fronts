c5b7bbf3d86674024b8df3822353595922aaa554
xiuxiu zhang <409410916@qq.com>
2019-10-28 19:26:59 +0800

    阿迪股份

--- didi-doctor/config/index.js
+++ didi-doctor/config/index.js
@@ -1,6 +1,6 @@
 // export const serverUrl="";
-const serverUrl="http://127.0.0.1:9999";
-const serverPatientUrl="http://127.0.0.1:9002";
+const serverUrl="https://doctor.dididoctor.cn";
+const serverPatientUrl="https://patient.dididoctor.cn";
 
 module.exports = {
 	serverUrl,
